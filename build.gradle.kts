1d51e2de956e61dac5a62b9c4f2d7fafc885ae75
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-03-09 13:26:23 +0100

    SubKt, NCs: Update build script, properties, common fonts, rename OP/ED directories, fonts
    Build script:
    - Add NC mux support
    - Add ktemplate generation and removal support
    - Force script info
    - Force video/audio info
    - Skip unused fonts
    - Create common fonts directory
    
    Properties:
    - Add muxout_pre
    - Change output directory and name
    - Rename OP/ED directories
    - Add common fonts directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,21 +9,53 @@ plugins {
     id("myaa.subkt")
 }
 
+fun String.isKaraTemplate(): Boolean {
+	return this.startsWith("code") || this.startsWith("template") || this.startsWith("mixin")
+}
+
+fun EventLine.isKaraTemplate(): Boolean {
+	return this.comment && this.effect.isKaraTemplate()
+}
+
 subs {
     readProperties("sub.properties")
     episodes(getList("episodes"))
+
+
+	val op_ktemplate by task<Automation> {
+        if (propertyExists("OP")) {
+            from(get("OP"))
+        }
+
+        video(get("premux"))
+		script("0x.KaraTemplater.moon")
+		macro("0x539's Templater")
+		loglevel(Automation.LogLevel.WARNING)
+	}
+
+
+	val ed_ktemplate by task<Automation> {
+        if (propertyExists("ED")) {
+            from(get("ED"))
+        }
+
+        video(get("premux"))
+		script("0x.KaraTemplater.moon")
+		macro("0x539's Templater")
+		loglevel(Automation.LogLevel.WARNING)
+	}
 
     merge {
         from(get("dialogue"))
 
         if (propertyExists("OP")) {
-            from(get("OP")) {
+            from(op_ktemplate.item()) {
                 syncTargetTime(getAs<Duration>("opsync"))
             }
         }
 
         if (propertyExists("ED")) {
-            from(get("ED")) {
+            from(ed_ktemplate.item()) {
                 syncTargetTime(getAs<Duration>("edsync"))
             }
         }
@@ -36,24 +68,44 @@ subs {
             from(get("TS"))
         }
 
-        out(get("mergedname"))
+        includeExtraData(false)
+        includeProjectGarbage(false)
+
+        scriptInfo {
+            title = "Kaleido-subs"
+            scaledBorderAndShadow = true
+        }
     }
+
+	val cleanmerge by task<ASS> {
+		from(merge.item())
+    	ass {
+			events.lines.removeIf { it.isKaraTemplate() }
+	    }
+	}
 
     chapters {
-        from(merge.item())
+        from(cleanmerge.item())
         chapterMarker("chapter")
     }
-
 
     mux {
         title(get("title"))
 
-        from(get("premux")) {
+		from(get("premux")) {
+			video {
+				lang("jpn")
+				default(true)
+			}
+			audio {
+				lang("jpn")
+				default(true)
+			}
             includeChapters(false)
 			attachments { include(false) }
-        }
+		}
 
-		from(merge.item()) {
+		from(cleanmerge.item()) {
 			tracks {
 				lang("eng")
                 name(get("group"))
@@ -64,6 +116,12 @@ subs {
 		}
 
         chapters(chapters.item()) { lang("eng") }
+
+        skipUnusedFonts(true)
+
+        attach(get("common_fonts")) {
+            includeExtensions("ttf", "otf")
+        }
 
         attach(get("fonts")) {
             includeExtensions("ttf", "otf")
@@ -82,5 +140,82 @@ subs {
         }
 
         out(get("muxout"))
+    }
+
+    tasks(getList("ncs")) {
+        // will create mux.ncop1, mux.ncop2, etc tasks
+        val run_ktemplate by task<Automation> {
+            from(get("ncsubs"))
+
+            video(get("ncpremux"))
+            script("0x.KaraTemplater.moon")
+            macro("0x539's Templater")
+            loglevel(Automation.LogLevel.WARNING)
+        }
+
+        merge {
+            from(run_ktemplate.item())
+
+            includeExtraData(false)
+            includeProjectGarbage(false)
+
+            scriptInfo {
+                title = "Kaleido-subs"
+                scaledBorderAndShadow = true
+            }
+        }
+
+        val cleanmerge by task<ASS> {
+            from(merge.item())
+            ass {
+                events.lines.removeIf { it.isKaraTemplate() }
+            }
+        }
+
+        chapters {
+            from(cleanmerge.item())
+            chapterMarker("chapter")
+        }
+
+        mux {
+            title(get("title"))
+
+            from(get("ncpremux")) {
+                video {
+                    lang("jpn")
+                    default(true)
+                }
+                audio {
+                    lang("jpn")
+                    default(true)
+                }
+                includeChapters(false)
+                attachments { include(false) }
+            }
+
+            from(cleanmerge.item()) {
+                tracks {
+                    lang("eng")
+                    name(get("group"))
+                    default(true)
+                    forced(true)
+                    compression(CompressionType.ZLIB)
+                }
+            }
+
+            chapters(chapters.item()) { lang("eng") }
+
+            skipUnusedFonts(true)
+
+            attach(get("common_fonts")) {
+                includeExtensions("ttf", "otf")
+            }
+
+            attach(get("fonts")) {
+                includeExtensions("ttf", "otf")
+            }
+
+            out(get("muxout"))
+        }
     }
 }
